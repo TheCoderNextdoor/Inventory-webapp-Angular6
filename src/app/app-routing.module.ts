e7f6d7f9c4074fbc33071245107db920998ca330
TheCoderNextdoor <a.banerjee6762@gmail.com>
2018-09-22 14:22:35 +0530

    c

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,15 @@ import { RouterModule, Routes, Router } from '@angular/router';
 import { ItemsComponent } from './items/items.component';
 import { Item } from './item';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 // an Angular route has two properties, path and component
 const routes: Routes = [
-  { path: 'items', component: ItemsComponent }
+  { path: 'items', component: ItemsComponent },
+  // path to the dashboard
+  { path: 'dashboard', component: DashboardComponent },
+  // this is the default route
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
