2502bab588fbd3d7095d200ad31bc00585e92974
TheCoderNextdoor <a.banerjee6762@gmail.com>
2018-09-22 23:09:20 +0530

    edited the mock items

--- src/app/mock-items.ts
+++ src/app/mock-items.ts
@@ -2,9 +2,10 @@ import { Item } from './item';
 
 // these are some mock items
 export const ITEMS: Item[] = [
-    {id: 1, name: 'pen', weight: 5},
-    {id: 2, name: 'book', weight: 20},
-    {id: 3, name: 'notebook', weight: 15},
-    {id: 4, name: 'laptop', weight: 40},
-    {id: 5, name: 'smartphone', weight: 12}
+    {id: 1, name: 'Sunglasses', weight: 5},
+    {id: 2, name: 'Book', weight: 20},
+    {id: 3, name: 'Notebook', weight: 15},
+    {id: 4, name: 'Laptop', weight: 40},
+    {id: 5, name: 'Smartphone', weight: 12},
+    {id: 6, name: 'Earphones', weight: 18},
 ];
